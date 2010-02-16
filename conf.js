cff18c6d1d9892062ce0c00cf89deae7c64421a6
unknown <kyo@.(none)>
2010-02-16 11:30:37 +0900

    タグの切り出し範囲を指定できるよう修正

--- conf.js
+++ conf.js
@@ -2,3 +2,18 @@ $.extend(mist.conf, {
 	'index_page' : '/index.html',
 	'api_url' : 'http://github.com/kyo-ago/mist.js/raw/master/test/'
 });
+/*
+その他の設定情報
+$.extend(mist.conf, {
+	'index_page' : '/index.html',
+	'api_url' : 'http://github.com/kyo-ago/mist.js/raw/master/test/',
+	// オーナーにアプリの所有を要求しない 
+	'OWNER_REQUIRE_APP_URL' : undefined,
+	// ビュアーにアプリの所有を要求しない 
+	'VIEWER_REQUIRE_APP_URL' : undefined,
+	// オーナーとビュアーが同じであることを要求しない 
+	'REQUIRE_OWNER_EQ_VIEWER_URL' : undefined,
+	// 切り出すタグの範囲 
+	'mist_page_filter_strip_mist_content_tags' : ['<div id="mist_content">', '<!-- /#mist_content --></div>']
+});
+*/
